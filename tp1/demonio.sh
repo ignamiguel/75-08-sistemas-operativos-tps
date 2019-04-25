8dc2d77e8f1dcb63b2620a7c5ac31f6e44bb6cb2
agustin <zurettiagustin@gmail.com>
2019-04-25 15:10:40 -0300

    agrego el proceso y varios

---
+++ tp1/demonio.sh
@@ -0,0 +1,125 @@
+#! /bin/bash
+TPPATH="$PWD"
+CONF="$TPPATH/conf"
+LOG="$CONF/log"
+
+NOVEDADES_PATH="$TPPATH/NOVEDADES"
+ACEPTADOS_PATH="$TPPATH/ACEPTADOS"
+RECHAZADOS_PATH="$TPPATH/RECHAZADOS"
+CONTADOR=0
+
+log ()
+{
+        date +"$USER-%x-%X-$1-$2" >> "$LOG/proceso.log" 
+}
+
+validarNombreTipo()
+{
+	
+	find "$NOVEDADES_PATH" -type f -not -name "Entregas_[0-9][0-9]" |
+		while read nombres
+			do
+				if [ -f "$nombres" ]
+				then
+					mv "$nombres" "$RECHAZADOS_PATH"
+					log "$nombres es un nombre incorrecto"
+				fi
+		done
+	
+	
+	for f in "$NOVEDADES_PATH"/*
+	do
+		VALIDO=true 
+
+		if ! [ -s $f ] 
+		then
+			
+			log "$f está vacio"
+			VALIDO=false
+		fi 
+
+		if ! [ -f $f ] 
+		then
+			VALIDO=false
+			log "$f no es un archivo regular"
+	 	fi
+
+	  	
+		if [ -f "$PROCESADOS_PATH/$(basename "$f")" ] 
+		then
+			log "$f fue procesado con anterioridad"
+			VALIDO=false
+		fi
+		
+
+		if [ $VALIDO = false ]
+		then
+			log "$f ha sido movido a rechazados"
+			mv $f "$RECHAZADOS_PATH"
+			
+		
+		else
+			log "$f ha sido movido a aceptado"	
+			mv $f "$ACEPTADOS_PATH"
+			
+			
+	 	fi
+	done
+}
+
+validarTrailer()
+{
+	
+	for f in "$ACEPTADOS_PATH"/* 
+	do
+	  
+	  lineas=-1 
+	  cp_total=0
+	  trailer_cantidad_lineas=0
+	  trailer_codigo_postal=0
+	  
+	  while IFS=',' read -r  operador pieza nombre doc_tipo doc_numero codigo_postal;
+	  do
+		let "lineas=lineas + 1"
+		let "cp_total=cp_total + codigo_postal"
+		trailer_codigo_postal=$codigo_postal
+		trailer_cantidad_lineas=$doc_numero
+	  done < $f
+	  #Comparo cantidad de lineas del archivo y suma codigo postal con los trailers
+	  let "codigo_postal_suma=codigo_postal_suma -trailer_codigo_postal " #le resto porque me suma el ultimo dos veces
+	  if [ $cantidad_lineas -eq $trailer_cantidad_lineas ] && [ $codigo_postal_suma -eq $trailer_codigo_postal ];
+	  then
+		log "El trailer de $f es correcto"
+	  else
+		log "El trailer de $f es incorrecto"
+		mv $f $RECHAZADOS_PATH
+	  fi
+	done
+}
+
+
+while true
+do
+	CONTADOR=$((CONTADOR+1))
+	log "ciclo:" "$CONTADOR"
+
+	#Verifico que sean validos los archivos de novedades
+	#Mando a aceptados o rechazados segun corresponda
+	if [ "$(ls -A "$NOVEDADES_PATH")" ]
+	then	
+		validarNombreTipo
+	fi
+
+	if [ "$(ls -A "$ACEPTADOS_PATH")" ]
+	then	
+		validarTrailer
+	fi
+
+	#Proceso los archivos de aceptados
+	#if [ "$(ls -A "$ACEPTADOS_PATH")" ]
+	#then	
+	#	procesamiento
+	#fi
+
+	sleep 1m
+done
