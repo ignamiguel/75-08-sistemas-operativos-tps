40c936b9aa49b5cce7912a8c788c44a616ccb020
Mauro Di Pietro <mauro.pietro@mercadolibre.com>
2019-06-02 23:13:24 -0300

    Cambios en semaforos

---
+++ tp2/portada.h
@@ -0,0 +1,10 @@
+void portada(){
+    cout<<"75.08 Sistemas Operativos"<<endl;
+    cout<<"Trabajo Practico N°2"<<endl;
+    cout<<"Tema 3 - Ascensor"<<endl;
+    cout<<endl;
+    cout<<"93965 Mauro Di Pietro"<<endl;
+    cout<<"95605 Agustín Zuretti"<<endl;
+    cout<<"95050 Ignacio Iglesias"<<endl;
+    cout<<endl;
+}
---
+++ tp2/terminador.cpp
@@ -0,0 +1,19 @@
+#include  "lib/sv_sem.h"
+#include  "lib/sv_shm.h"
+#include  "portada.h"
+using namespace std;
+
+int main(){
+
+    portada();
+
+    sv_sem cliente ("cliente",0);
+    sv_sem barbero ("barbero",0);
+    sv_shm area("area");
+    cliente.del();
+	cout<<"Semaforo cliente borrado"<<endl;
+    barbero.del();
+	cout<<"Semaforo barbero borrado"<<endl;
+    area.del();
+	cout<<"Area compartida borrada"<<endl;
+}
